9d95891f01599200ac278101ef2edca31849a272
jan <samjan1127@gmail.com>
2023-03-04 11:07:51 +0900

    re init

---
+++ src/test/java/com/sparta/snackback/SnackbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.snackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
