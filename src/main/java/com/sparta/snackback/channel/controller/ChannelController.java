acc7cccd4e02ab1b9eb87d3ea99d1b74ba5153b7
jan <samjan1127@gmail.com>
2023-03-07 13:34:43 +0900

    Feat: 채널 전체 조회 #8

--- src/main/java/com/sparta/snackback/channel/controller/ChannelController.java
+++ src/main/java/com/sparta/snackback/channel/controller/ChannelController.java
@@ -5,10 +5,9 @@ import com.sparta.snackback.channel.service.ChannelService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -24,5 +23,10 @@ public class ChannelController {
         return channelService.createChannel(request);
     }
 
+    // 채널 전체 조회
+    @GetMapping
+    public ResponseEntity<List<ChannelRoomDto.Response>> getChannels() {
+        return channelService.showChannels();
+    }
 
 }
