a8136f4804a29a388d44b58c15af11d51a51c86b
jan <samjan1127@gmail.com>
2023-03-09 18:15:25 +0900

    Feat: update 쿼리 추가

--- src/main/java/com/sparta/snackback/user/repository/UserRepository.java
+++ src/main/java/com/sparta/snackback/user/repository/UserRepository.java
@@ -2,9 +2,17 @@ package com.sparta.snackback.user.repository;
 
 import com.sparta.snackback.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE users SET image = ?1, nickname = ?2 WHERE id = ?3 ")
+    int findById(String image, String nickname, Long id);
 }
